ad72a278b4b33b2d52409ce22c9256c44d6df81e
PhantomDesignz-Chris <67562484+PhantomDesignz-Chris@users.noreply.github.com>
2025-04-25 15:33:23 -0400

    Update autoloader.php

--- image-optimizer-pro/includes/autoloader.php
+++ image-optimizer-pro/includes/autoloader.php
@@ -1,16 +1,35 @@
 <?php
 spl_autoload_register(function ($class_name) {
-    $namespace = 'Image_Optimizer_Pro';
+    $namespace = 'Image_Optimizer_Pro\\';
     
+    // Only autoload our plugin's classes
     if (strpos($class_name, $namespace) !== 0) {
         return;
     }
     
+    // Remove namespace prefix
     $class_name = str_replace($namespace, '', $class_name);
-    $class_name = str_replace('\\', DIRECTORY_SEPARATOR, $class_name);
-    $class_file = IOP_PLUGIN_DIR . 'includes' . $class_name . '.php';
     
-    if (file_exists($class_file)) {
-        require_once $class_file;
+    // Convert to file path
+    $class_file = str_replace('_', '-', strtolower($class_name)) . '.php';
+    
+    // Possible file paths
+    $locations = [
+        IOP_PLUGIN_DIR . 'includes/',
+        IOP_PLUGIN_DIR . 'includes/classes/'
+    ];
+    
+    // Try each location
+    foreach ($locations as $location) {
+        $file = $location . $class_file;
+        if (file_exists($file)) {
+            require_once $file;
+            return;
+        }
+    }
+    
+    // Log if class not found (but don't break the site)
+    if (defined('WP_DEBUG') && WP_DEBUG) {
+        error_log("Image Optimizer Pro: Class {$class_name} not found in {$class_file}");
     }
 });
